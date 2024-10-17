ff74c681e6b72fa131d5ed5d98ea9a3f6b94aab0
kkwasny <kkwasny@student.42wolfsburg.de>
2024-10-17 14:13:33 +0200

    step 4; didn't commit step 3 but it was simmilar

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -20,9 +20,9 @@ const Content = (props) => {
   console.log(props)
   return(
     <div>
-      <Part parts={props.parts[0]} exercises={props.exercises[0]} />
-      <Part parts={props.parts[1]} exercises={props.exercises[1]} />
-      <Part parts={props.parts[2]} exercises={props.exercises[2]} />
+      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
@@ -36,23 +36,28 @@ const Total = (props) => {
   )
 }
 
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [ part1, part2, part3 ]
-  const exercises = [ exercises1, exercises2, exercises3 ]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts} exercises={exercises} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
